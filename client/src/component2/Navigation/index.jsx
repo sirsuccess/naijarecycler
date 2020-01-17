47a4a83508e7684af757198da78d298d232f85bd
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-01-17 14:01:44 +0100

    change nav background

--- client/src/component2/Navigation/index.jsx
+++ client/src/component2/Navigation/index.jsx
@@ -1,31 +1,58 @@
-
-
-import React from "react"
-import "./Navigation.css"
-import { Navbar, Nav, Button } from "react-bootstrap"
-
-
+import React from "react";
+import "./Navigation.css";
+import { Navbar, Nav, Button } from "react-bootstrap";
 
 function Navigation() {
-    return (
-        <div>
-                <Navbar bg="dark" expand="lg" className="nav-bg">
-                    <Navbar.Brand href="/"><img className="logo1" src="https://res.cloudinary.com/dtbjhs8a6/image/upload/v1576094329/f22ietnqwtslcbasqym1.png" alt="" width="150" height="50" /></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/" style={{color: "red", fontWeight: "bold",marginLeft: "3rem"}}>Home</Nav.Link>
-                            <Nav.Link href="/about"  style={{color: "grey", fontWeight: "bold", marginLeft: "1rem"}}>About Us</Nav.Link>
-                            <Nav.Link href="/waste-collection"  style={{color: "grey", fontWeight: "bold", marginLeft: "1rem"}}>Waste Collection</Nav.Link>
-                            <Nav.Link href="/recycling"  style={{color: "grey", fontWeight: "bold", marginLeft: "1rem"}}>Recycling</Nav.Link>
-                            <Nav.Link href="/signup" style={{marginLeft: "1rem"}}><Button className="btn btn-success" >Sign Up</Button></Nav.Link>
-                            <Nav.Link href="/login" style={{marginLeft: "1rem"}}><Button className="btn btn-success" >Log In</Button></Nav.Link>                    
-                        </Nav>
-                        
-                    </Navbar.Collapse>
-                </Navbar>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar expand="lg" className="nav-bg bg-dark">
+        <Navbar.Brand href="/">
+          <img
+            className="logo1"
+            src="https://res.cloudinary.com/dtbjhs8a6/image/upload/v1576094329/f22ietnqwtslcbasqym1.png"
+            alt=""
+            width="150"
+            height="50"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link
+              href="/"
+              style={{ color: "red", fontWeight: "bold", marginLeft: "3rem" }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="/about"
+              style={{ color: "grey", fontWeight: "bold", marginLeft: "1rem" }}
+            >
+              About Us
+            </Nav.Link>
+            <Nav.Link
+              href="/waste-collection"
+              style={{ color: "grey", fontWeight: "bold", marginLeft: "1rem" }}
+            >
+              Waste Collection
+            </Nav.Link>
+            <Nav.Link
+              href="/recycling"
+              style={{ color: "grey", fontWeight: "bold", marginLeft: "1rem" }}
+            >
+              Recycling
+            </Nav.Link>
+            <Nav.Link href="/signup" style={{ marginLeft: "1rem" }}>
+              <Button className="btn btn-success">Sign Up</Button>
+            </Nav.Link>
+            <Nav.Link href="/login" style={{ marginLeft: "1rem" }}>
+              <Button className="btn btn-success">Log In</Button>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
-export default Navigation
+export default Navigation;
